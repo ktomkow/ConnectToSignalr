363d8758ab39c230997899d6858ac3677e525ca6
ktomkow <krzysztof.tomkow@gmail.com>
2020-11-28 16:17:16 +0100

    Installed sinalr

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Home from "./app/components/home/home";
 import NotFound from "./app/components/notFound/notFound";
+import Simple from './Components/Simple/simple';
 
 const App = () => {
   return (
     <Router>
       <Switch>
-        <Route path="" component={Home} />
+        <Route path="" component={Simple} />
         <Route component={NotFound} />
       </Switch>
     </Router>
---
+++ src/Components/Simple/simple.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Simple = () => {
+    return <div>Hello Dupa</div>
+}
+
+export default Simple;
